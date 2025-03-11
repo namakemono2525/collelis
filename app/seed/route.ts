294d26da6d461421b85c5ad1860ee3d95520f0b9
namakemono2525 <kaho.miyake5978@icloud.com>
2025-03-11 22:53:55 +0900

    vercel reduce

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,11 +1,12 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs'; 
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const sql: any = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function seedUsers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
   await sql`
     CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -30,7 +31,7 @@ async function seedUsers() {
 }
 
 async function seedInvoices() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
 
   await sql`
     CREATE TABLE IF NOT EXISTS invoices (
@@ -56,7 +57,7 @@ async function seedInvoices() {
 }
 
 async function seedCustomers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
 
   await sql`
     CREATE TABLE IF NOT EXISTS customers (
